a179d1908d12ca5d11310442f9e053a4e5e55456
Stanislav Pyatnicyn <pyatnicyn.stanislav@gmail.com>
2022-02-17 21:17:32 +0300

    Generation simple pixel background

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import P5 from 'p5';
+import 'p5/lib/addons/p5.dom';
+import { HEIGHT, WIDTH } from './consts';
+
+import PixelArt from './PixelArt';
+
+const sketch = (p5: P5) => {
+  p5.setup = () => {
+    p5.createCanvas(WIDTH, HEIGHT);
+
+    new PixelArt(p5).draw();
+  };
+
+  p5.draw = () => {};
+};
+
+new P5(sketch);
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+export const PIXEL_SIZE = 10;
+export const ROWS = 64;
+export const COLUMNS = 64;
+
+export const WIDTH = PIXEL_SIZE * COLUMNS;
+export const HEIGHT = PIXEL_SIZE * ROWS;
